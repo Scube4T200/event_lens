c08d6a3c96028b8226d2f4cc9b52b3a651737a70
Shiv Sankar Sarkar <shiv2armail@gmail.com>
2026-07-19 18:05:02 +0530

    Set Up Initial React Application

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>EventLens</h1>
+
+      <p>
+        See the facts.
+        <br />
+        Understand the perspectives.
+        <br />
+        Decide for yourself.
+      </p>
+    </main>
+  )
+}
+
+export default App
